b00f584e136fdb6fe9f76038b2f5261bbf94af1c
matthewwcarlson <72997319+matthewwcarlson@users.noreply.github.com>
2020-11-09 19:15:07 -0800

    Add in Limit Switch and Fan Driver
    Limit Switch file that works with the Debouncer files
    Fan Driver is a simple motor driver
    // Need to comment out void setup () and task definitions and put them in the main.cpp

---
+++ src/debouncer.cpp
@@ -0,0 +1,151 @@
+/** @file debouncer.cpp
+ *    This file contains the source code for a class that contains a debouncer program
+ *    so that we can read through some noise from button pushes and limit switches
+ * 
+ *  @author Matthew Carlson
+ *  @date  2020-Oct-24 Original file
+ */
+
+
+/** @brief   Class which implements a debouncer for button and switch noise
+ *  @details Mostly defines the function "update" and impliments the class into the constructor 
+ * 
+ */
+
+#include <Arduino.h>
+#include <PrintStream.h>
+#include "debouncer.h"
+
+
+/** @brief   Creates a debouncer to filter through noise from buttons and switches
+ *  @details This contructor looks at a data stream, identifies when the a signal has
+ *           been activated, and switches state. It sets up an State Machine using a set 
+ *           of IF statements to determine what the values are of an input pin, counts 
+ *           how many consecutive 0's or 1's are detected, and what corresponding state
+ *           it should be in. State 0 outputs a FALSE and State 2 outputs a TRUE.
+ *  @param   pin This is the parameter of which pin our button or switch is connected to
+ *  @param   threshold This is set as whatever we want our threshold to be for how many consecutive
+ *                     1's or 0's are acceptable for saying the button or switch is active or not.
+ */
+Debouncer::Debouncer (uint8_t pin, uint8_t threshold_constructor)
+{
+    // define a variable input_pin that reads either a HIGH (1) or LOW (0) from
+    // the blue push botton (PC13) on the Nucleo L476
+    input_pin = pin;
+
+    // Set some number of 0's or 1's in a row to be the threshhold for if the switch is activated
+    threshold = threshold_constructor;
+
+
+    // Set up button as in input using PinMode
+    pinMode(input_pin, INPUT_PULLUP);
+
+}
+
+
+
+// Define "update" function
+bool Debouncer::update (void)
+{
+    bool signal = digitalRead(input_pin);       // Read HIGH or LOW from input_pin
+
+
+
+        if (state == 0)                         // State 0: Waiting and reading input pin: PC13
+        {
+           button_result = 0;                   // Set "result" equal to FALSE in State 0
+
+           Serial << "Waiting...                  " << "\r";
+
+
+            if (signal == 0)                     // Read pin: PC13 (Active state is Logic 0)
+            {
+                state = 1;                          // IF zero is detected, go to state 1
+            Serial << "Going to Counting...    " << "\r";
+            }
+        }
+
+        
+        else if (state == 1)                    // State 1: Counting number of zeros, if enough go 
+                                                // to State 2, if not go back to State 1, if there's
+                                                // a 1, go back to State 0
+        {
+            Serial << "Counting...              " << "\r";
+
+            if (++counter >= threshold)         // Compare "counter" to "threshhold"
+            {
+                state = 2;                      // If threshhold is met, go to state 2
+                counter = 0;                    // Reset counter so it can be used again
+                Serial << "Going to Triggered...  " << "\r";
+            }
+
+            else if (signal == 1)
+            {
+                state = 0;                      // IF one is detected, go back to state 0
+                counter = 0;                    // Reset counter
+                Serial << "Going to Waiting...   " << "\r";
+
+            }
+
+            else
+            {
+                state = 1;                      // ELSE keep state = 1 so it can run through
+                                                // and add to the "++" the counter again
+                
+            }
+        }
+
+
+        else if (state == 2)                    // State 2: Triggered
+        {
+            // Returns a TRUE
+
+            button_result = 1;                  // Set "result" equal to TRUE in State 2
+
+            Serial << "Triggered!!               " << "\r";
+
+
+
+
+            if (signal == 1)                    // Do another counter so we can reset to State 0 
+                                                // if there are a bunch of Logic 1's in a row
+            {
+
+                if (++counter >= threshold)     // Compare "counter" to "threshhold"
+                {
+                    state = 0;                  // If threshhold is met, reset back to State 0
+                    counter = 0;                // Reset counter so it can be used again
+                    Serial << "Going to Waiting...     " << "\r";
+
+                }
+                
+                else
+                {
+                    state = 2;                  // Stays in State 2
+                }
+            }
+
+            else if (signal == 0)
+            {
+                counter = 0;                    // Reset counter when there is a zero
+                state = 2;                      // Stays in State 2
+
+
+            }
+
+
+        }
+
+        else
+        {
+            Serial << "There is a large problem happening, must reset to State 0" << "\r";
+            state = 0;
+
+        }
+
+    return button_result;
+}
+
+
+
+
---
+++ src/debouncer.h
@@ -0,0 +1,42 @@
+/** @file debouncer.h
+ *    This file contains the headers for a class that implements a debouncer program
+ *    so that we can read through some noise from button pushes and limit switches
+ * 
+ *  @author Matthew Carlson
+ *  @date  2020-Oct-24 Original file
+ */
+
+
+/** @brief   Class which implements a debouncer for button and switch noise
+ *  @details Defines Protected variables that are used in the debouncer.cpp file
+ *           Also sets up contructor "Debouncer" and function "update"
+ * 
+ */
+class Debouncer
+{
+protected:
+    /// The value of the threshold so that the debouncer knows when to switch states
+    uint8_t threshold;
+
+    /// The value of the threshold so that the debouncer knows when to switch states
+    uint8_t input_pin;
+
+    // State of the state machine
+    uint8_t state = 0;   
+
+    // define a variable "counter" that will be incrimented in a FOR loop and
+    // and compared to "threshhold"
+    uint8_t counter = 0;
+
+    // This should not be needed, but it is used so that State 0 returns a FALSE and State 2 returns a TRUE
+    bool button_result = 0;
+
+
+public:
+    // Constructor which is given a pin to look at and a threshold to compare against
+    Debouncer (uint8_t pin, uint8_t threshold_constructor);
+
+    bool update (void);
+    
+ 
+};
