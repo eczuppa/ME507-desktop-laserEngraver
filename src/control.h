921e2308d1decb5e424a11ea32a8fef35b630fb9
matthewwcarlson <72997319+matthewwcarlson@users.noreply.github.com>
2020-11-19 00:30:51 -0800

    Preliminary testing of the controller with single line
    Testing the controller using a dummy single line of code. Fixed useless variables in the control.cpp file and other random bugs. Added the delta_time input to the controller.

--- src/control.h
+++ src/control.h
@@ -52,14 +52,15 @@ protected:
     // Position Desired and Actual:
 
     float pos_desired;          // comes from the GCode (setpoint/target)
-    float pos_actual;           // comes from the encoder (feedback)
+    float pos_actual;           // comes from the encoder (feedback) (Current)
 
 
     // Velocity Desired and Actual:
 
     float vel_desired;          // comes from the GCode (setpoint/target)
-    float vel_actual;           // comes from the encoder (feedback)
+    float vel_actual;           // comes from the encoder (feedback) (Current)
 
+    float delta_time;           // comes from the encoder
 
     // // The value for the Setpoint, this is the commanded position from GCode
     // float setpoint;                     // potentially make this a large integer instead of a float
@@ -70,21 +71,25 @@ protected:
     // // This will be in terms of linear A and B that will be calculated in the kinematics module 
 
 
-    // This is the current actual position and velocity (feedback) that is 
-    // continually being read by the encoder.
-    float pos_actual_current;
-    float vel_actual_current;
+    // // This is the current actual position and velocity (feedback) that is 
+    // // continually being read by the encoder.
+    // float pos_actual_current;
+    // float vel_actual_current;
+
+    // These are the position and velocity from the previous iteration
+    float pos_last;
+    float vel_last;
 
     // time will come from our overall timer (using millis or something)
     float current_time;
     float last_time;
 
-    // The error will be computed as th difference between the Desired and the Actual
+    // The error will be computed as the difference between the Desired and the Actual
     float pos_error;
-    float pos_last_error;
+    float pos_error_last;
 
     float vel_error;
-    float vel_last_error;
+    float vel_error_last;
 
     float integral_cumulation;
     float integral_cumulation_max;
@@ -101,7 +106,7 @@ public:
     // ----- Define the Constructor -----
 
     // Constructor for Controller_P that takes in a gain, position desired, position actual, velocity desired, and velocity actual)
-    Controller_PID (float kP_gain, float kI_gain, float kD_gain, float pos_desired_initial, float pos_actual_initial, float vel_desired_initial, float vel_actual_initial);              
+    Controller_PID (float kP, float kI, float kD, float pos_desired_initial, float pos_actual_initial, float vel_desired_initial, float vel_actual_initial, float delta_time_initial);              
     // Note: make these input values equal to the Protected variables in the .cpp file
 
 
@@ -118,9 +123,10 @@ public:
 
 
 
+    // Gain's
 
     // set_gain_PID allows the user to input kP, kI and kD
-    void set_gain_PID (float gain_kP_new, float gain_kI_new, float gain_kD_new);
+    void set_gain_PID (float kP, float kI, float kD);
 
     // set_gain_kP allows the user to input a new gain value. Allows us to change the Protected variable
     void set_gain_kP (float gain_kP_new);        // This will be for testing only
@@ -143,24 +149,46 @@ public:
     float get_gain_kD ();                        // This will be for testing only
  
 
-    // set_pos_desired_PID allows the GCode to continually feed in new position commands
-    void set_pos_desired_PID (float pos_desired_new);        // This will allow the GCode to overwrite it's previous commands
 
-    // get_pos_desired_PID helps with testing
-    float get_pos_desired_PID ();
+    // Position Desired and Actual
+
+    // set_pos_desired allows the GCode to continually feed in new position commands
+    void set_pos_desired (float pos_desired_initial);       // This will allow the GCode to overwrite it's previous commands
+
+    // get_pos_desired helps with testing
+    float get_pos_desired ();
+
+    // set_pos_actual allows the encoder to continually feed in new position observations
+    void set_pos_actual (float pos_actual_initial);         // This will allow the encoder to overwrite it's previous commands
+
+    // get_pos_actual helps with testing
+    float get_pos_actual ();
 
 
-    // set_vel_desired_PID allows the GCode to continually feed in new velocity commands
-    void set_vel_desired_PID (float vel_desired_new);        // This will allow the GCode to overwrite it's previous commands
 
-    // get_vel_desired_PID helps with testing
-    float get_vel_desired_PID ();
+    // Velocity Desired and Actual
+
+    // set_vel_desired allows the GCode to continually feed in new velocity commands
+    void set_vel_desired (float vel_desired_initial);       // This will allow the GCode to overwrite it's previous commands
+
+    // get_vel_desired helps with testing
+    float get_vel_desired ();
+    
+    // set_vel_actual allows the encoder to continually feed in new velocity observations
+    void set_vel_actual (float vel_actual_initial);         // This will allow the encoder to overwrite it's previous commands
+
+    // get_vel_actual helps with testing
+    float get_vel_actual ();
+
+
+
+    // Output's
 
     // get_output displays the output from the PID loop
-    float get_output ();                               // Value will be fed to the motors, PWM
+    float get_output ();                                    // Value will be fed to the motors, PWM
 
     // get_pos_error displays the position error from the PID loop
-    float get_pos_error ();                               // Value of position error
+    float get_pos_error ();                                 // Value of position error
 
 
 
--- src/share_testing.cpp
+++ src/share_testing.cpp
@@ -14,22 +14,24 @@
 // extern Share<float> GCode_share_vel;
 // extern Share<float> encoder_share_pos;
 // extern Share<float> encoder_share_vel;
+// extern Share<float> encoder_share_time;
 
 
 // void share_testing_send (void* p_params)
 // {
 //     (void)p_params;            // Does nothing but shut up a compiler warning
 
-//     Serial << "PRINT ME share_testing.cpp file" << endl;
+//     // Serial << "PRINT ME share_testing.cpp file" << endl;
 
 //     // Set up a singular line of GCode that will go into the control loop for motor A
 //     float GCode_A_pos = 4;              // this will go into a share GCode_share that will be the "desired" input for position
 //     float GCode_A_vel = 10;             // this will go into a share GCode_share that will be the "desired" input for velocity
 
 //     // Set up a singular line of encoder code that will go into the control loop for motor A
-//     float encoder_A_pos = 40.2;          // this will go into a share Encoder_share that will be the "actual" input for position
-//     float encoder_A_vel = 100.2;         // this will go into a share Encoder_share that will be the "actual" input for velocity
+//     float encoder_A_pos = 4.2;          // this will go into a share Encoder_share that will be the "actual" input for position
+//     float encoder_A_vel = 10.2;         // this will go into a share Encoder_share that will be the "actual" input for velocity
 
+//     float encoder_A_time = 100;         // this will go into the share Encoder_share that will be the "current" delta time
 
 //     for (;;)
 //     {
@@ -47,6 +49,7 @@
 //         GCode_share_vel.put (GCode_A_vel);          // put data into share
 //         encoder_share_pos.put (encoder_A_pos);      // put data into share
 //         encoder_share_vel.put (encoder_A_vel);      // put data into share   
+//         encoder_share_time.put (encoder_A_time);      // put data into share
 
 //         // Timing accuracy isn't extremely important, so use the simpler delay
 //         vTaskDelay (100);
