666fee09859b036f4395176b8f27024a4560a1bf
eczuppa <ethan.czuppa@gmail.com>
2020-11-22 19:03:57 -0800

    Motionplanning code done and should work
    more doxygen comments needed, and some testing is required but other than figuring out how to get to all the elements using the class member data which points to the array and some memory management stuff we will have to do after every ramp input has been succesfully recieved by the control task, we are ready for testing this class' functionality

--- src/motionplanning.cpp
+++ src/motionplanning.cpp
@@ -25,57 +25,67 @@ MotionPlanning::MotionPlanning(float setpoint_current, float setpoint_last, floa
     _setpoint_last = setpoint_last;
     _feed_setpoint = feed_setpoint;
     _ramp_dt_period = ramp_dt_period;
+    _output_ramp = NULL;
 
 }
 
-void MotionPlanning::ramp_generator(void)
-{
-    // calculates the time required to move from last to current point in microseconds
-    float dist; 
-    uint32_t ramp_sections = 0;
-    uint32_t no_ramp;
-    uint32_t index;
-    bool even = false;
-    int8_t aug_index = 0;
 
+void MotionPlanning::init_ramp(void)
+{
+    float dist; 
+    //uint32_t no_ramp
     dist = abs(_setpoint_current - _setpoint_last);
     _tot_travel_period = (dist*1000000)/_feed_setpoint; // travel period in microseconds
     
     // The time the movement takes between the current line of Gcode and the last is greater than the ramp section period
     // thus, we want to make the ramp input so our controller continues to behave in a linear manner and is not subjected to
     // large disturbances
-    
-    
-    if (_tot_travel_period >= _ramp_dt_period)
+
+    // calculate number of ramp sections (close enough approx of euclid algorithm to get gcf using floating point version of %)
+    _ramp_sections = (_tot_travel_period - (fmodf(_tot_travel_period, _ramp_dt_period))) /_ramp_dt_period;   
+    // calculate the time where the ramp section of specified length would not fit
+    //no_ramp = _tot_travel_period - (_ramp_dt_period*ramp_sections);  
+    if (_tot_travel_period <= _ramp_dt_period)
     {
-        // calculate number of ramp sections (close enough approx of euclid algorithm to get gcf using floating point version of %)
-        ramp_sections = (_tot_travel_period - (fmodf(_tot_travel_period, _ramp_dt_period))) /_ramp_dt_period;   
-        // calculate the time where the ramp section of specified length would not fit
-        no_ramp = _tot_travel_period - (_ramp_dt_period*ramp_sections);  
         if (fmodf(_tot_travel_period, _ramp_dt_period) == 0)
         {
             // even division of ramp_dt_period into the total travel time, just make room for the current feed rate because the current setpoint will be included in the ramp
-            ramp_sections ++;
-            even = true;
+            _ramp_sections ++;
+            _even = true;
         }
         else 
         {
             // generated ramp stops short of current setpoint becasue remaining time less than ramp_dt_period
             // make room for current setpoint and feed rate
-            ramp_sections +=2;
+            _ramp_sections +=2;
         }
 
-        float ramp[ramp_sections];   // static arrays can't be variable in length, wrapping with a struct is proving quite complicated,
-                                     // figure out an easier way to do this or make it calculate just one piece at a time and send that to both controllers
+        _output_ramp = new float[_ramp_sections];   // the pointer to the dynamically allocated array can no be used like a normal array
+    }
+    
+    else if (_tot_travel_period < _ramp_dt_period)
+    {
+        _ramp_sections = 2;
+        _output_ramp = new float[_ramp_sections];
+    }
 
 
+}
 
+
+void MotionPlanning::ramp_generator(void)
+{
+    // calculates the time required to move from last to current point in microseconds
+    if (_tot_travel_period <= _ramp_dt_period)
+    {
+        int32_t index = 0;
+        int8_t aug_index = 1;
 
            // array to hold ramp input to controller
         // setup for generation of ramp
 
         // if even division only change index by one to make room for current feed 
-        if (even)
+        if (_even)
         {
             aug_index = 1;
         }
@@ -86,41 +96,44 @@ void MotionPlanning::ramp_generator(void)
         }
         
         // generate the ramp by iteration in a for loop
-        for (index=0; (index=(ramp_sections - aug_index)); index++)
+        for (index=0; (index=(_ramp_sections - aug_index)); index++)
         {
-            ramp[index] = ((_setpoint_current - _setpoint_last) * index * _ramp_dt_period) / _tot_travel_period;
+            _output_ramp[index] = ((_setpoint_current - _setpoint_last) * index * _ramp_dt_period) / _tot_travel_period;
         }
         
         // Add desired final value or values to the ramp 
-        if(even)
+        if(_even)
         {
-            memset(ramp,_feed_setpoint, ramp[sizeof(ramp)/sizeof(*ramp)]);           // set the last value of the array to the value of the feed_setpoint 
+            memset(_output_ramp,_feed_setpoint, _output_ramp[_ramp_sections]);           // set the last value of the array to the value of the feed_setpoint 
         }
         else
         {
-            memset(ramp,_setpoint_current, ramp[(sizeof(ramp)/sizeof(*ramp))-1]);    // set penultimate value of the array to the value of the current setpoint
-            memset(ramp,_feed_setpoint, ramp[sizeof(ramp)/sizeof(*ramp)]);           // set the last value of the array to the value of the feed_setpoint 
+            memset(_output_ramp,_setpoint_current, _output_ramp[_ramp_sections-1]);    // set penultimate value of the array to the value of the current setpoint
+            memset(_output_ramp,_feed_setpoint, _output_ramp[_ramp_sections]);           // set the last value of the array to the value of the feed_setpoint 
         
         }
-        
-        _output_ramp = &ramp[ramp_sections];                                    // set the class member data pointer output_ramp point to the generated ramp
+
     }
-    
+
     // For very short or fast movements over sufficiently small distances - like the linear interpolation of arcs below a certain size
     // do not make a ramp. Instead put the current position setpoint and the feed setpoint into the array and make the class member data pointer
     // output ramp point to it.
     else if (_tot_travel_period < _ramp_dt_period)
     {
-        int8_t ramp_length = 1;
-        float ramp[ramp_length];
-        ramp[0] = _setpoint_current;
-        ramp[1] = _feed_setpoint;
-        _output_ramp = &ramp[ramp_length];
+        _output_ramp[0] = _setpoint_current;
+        _output_ramp[1] = _feed_setpoint;
     }
 
 }
 
 float * MotionPlanning::get_ramp(void)
 {
-    return _output_ramp;
+    return _output_ramp;   // returns the persistent reference to the dynamically allocated array _output_ramp
+}
+
+void MotionPlanning::deinit_ramp(void)
+{
+    delete [] _output_ramp;   // deallocate memory pointed to by *_output_ramp
+    _output_ramp = NULL;      // make _ouput_ramp point back to NULL
+    _ramp_sections = 0;       // zero out ramp sections so it is ready for the next ramp input
 }
--- src/motionplanning.h
+++ src/motionplanning.h
@@ -12,24 +12,29 @@ class MotionPlanning
     // class member data to store the discretization period
     
     // a Motor 
-    float _setpoint_current;      // Stores current target position (in mm)
-    float  _setpoint_last;        // stores last target postion (in mm)
-    float _feed_setpoint;         // store the target feed rate for the motor
-    float _delta_dist;            // the distance between the current and last setpoints
-    float _tot_travel_period;    // the amount of time required to travel from last to current point based on the current feedrate
+    float _setpoint_current;        // Stores current target position (in mm)
+    float  _setpoint_last;          // stores last target postion (in mm)
+    float _feed_setpoint;           // store the target feed rate for the motor
+    float _delta_dist;              // the distance between the current and last setpoints
+    float _tot_travel_period;       // the amount of time required to travel from last to current point based on the current feedrate
     uint32_t _ramp_dt_period;       // the dt for each section of the ramp input
+    uint32_t _ramp_sections;        // the number of discrete sections of the ramp input 
     float *_output_ramp;            // the pointer to the float array of linearly interpollated portions of the overall movement specified between the lines of G-code
-
+    bool _even = false;             // boolean for how to adjust the index of the dynamically allocated array which stores the ramp input
 
 
 
     public:
     // a constructor that probably takes in the given array or individual variables X,Y,F current and last as well as the discretization period
     MotionPlanning(float setpoint_current, float setpoint_last, float feed_setpoint, uint32_t ramp_dt_period);
+    // a method to dynamically allocate memory for the ramp generator method
+    void init_ramp(void);
     // a method to calculate the discrete sections of the ramp input to the contorller based on the current and last lines of GCODE (from a completion standpoint - last is just completed, current is what will be)
     void ramp_generator(void);
     // a getter method to get the discrete movements 
     float * get_ramp(void);
+    // a method to delete the dynamically allocated memory when all of it has been queued up
+    void deinit_ramp(void);
 
 
 };
